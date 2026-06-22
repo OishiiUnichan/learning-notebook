9e639bbbe20db949bf27503b6eea898e466de207
OishiiUnichan <pekeko423@gmail.com>
2026-06-22 16:40:37 +0900

    見やすいように内容を整理

---
+++ 自主学習/JavaMasterLesson.java
@@ -0,0 +1,39 @@
+public class JavaMasterLesson {
+  
+  //1.フィールドとカプセル化
+  private String studentName;
+
+  //コンストラクタ
+  public JavaMasterLesson(String name){
+    this.studentName = name;
+  }
+
+  //メインメソッド
+  public static void main(String[] args) {
+    System.out.println("--Java学習スタート--");
+
+    //インスタンスの作成
+    JavaMasterLesson lesson = new JavaMasterLesson("Java初学者");
+
+    //2.基本的なデータ型と演算の実行
+    lesson.demonstrateDataType();
+
+    //3.条件分岐
+    lesson.demonstrateControlFlow(85);
+    lesson.demonstrateSwitch("Go");
+
+    //4.ループ処理
+    lesson.demonstrateLoops();
+
+    //5.配列操作
+    lesson.demonstrateArrays();
+
+    System.out.println("--全ての学習プログラムが終了しました--");
+  }
+
+  /**
+  * 2.基本的なデータ型
+  */
+
+  
+}
