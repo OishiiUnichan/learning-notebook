eae886248333d32cbbb281fc7801f5660069c797
OishiiUnichan <pekeko423@gmail.com>
2026-06-22 19:16:40 +0900

    追加

--- 自主学習/JavaMasterLesson.java
+++ 自主学習/JavaMasterLesson.java
@@ -1,39 +1,42 @@
 public class JavaMasterLesson {
-  
-  //1.フィールドとカプセル化
+
+  // 1.フィールドとカプセル化
   private String studentName;
 
-  //コンストラクタ
-  public JavaMasterLesson(String name){
+  // コンストラクタ
+  public JavaMasterLesson(String name) {
     this.studentName = name;
   }
 
-  //メインメソッド
+  // メインメソッド
   public static void main(String[] args) {
     System.out.println("--Java学習スタート--");
 
-    //インスタンスの作成
+    // インスタンスの作成
     JavaMasterLesson lesson = new JavaMasterLesson("Java初学者");
 
-    //2.基本的なデータ型と演算の実行
+    // 2.基本的なデータ型と演算の実行
     lesson.demonstrateDataType();
 
-    //3.条件分岐
+    // 3.条件分岐
     lesson.demonstrateControlFlow(85);
     lesson.demonstrateSwitch("Go");
 
-    //4.ループ処理
+    // 4.ループ処理
     lesson.demonstrateLoops();
 
-    //5.配列操作
+    // 5.配列操作
     lesson.demonstrateArrays();
 
     System.out.println("--全ての学習プログラムが終了しました--");
   }
 
   /**
-  * 2.基本的なデータ型
-  */
+   * 2.基本的なデータ型
+   */
 
-  
+  public void demonstrateDataType() {
+    System.out.println("\n[2.データ型と演算]");
+  }
+
 }
