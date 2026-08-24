68df169744c3cee1c082dfa1f3f94ef9dabc5436
OishiiUnichan <pekeko423@gmail>
2026-08-24 12:01:42 +0900

    文字列操作に関する例を追加し、三重引用符、連結、format()、f文字列の使用法を説明

---
+++ Python/code/inch-to-cm-fstring.py
@@ -0,0 +1,19 @@
+# インチをcmに変換
+per_inch = 2.54
+inch = 24
+cm = inch * per_inch
+
+# 文字列で説明を加える
+desc = f"{inch}インチ={cm}センチ"
+print(desc)
+
+print("--------")
+
+# インチをcmに変換
+per_inch = 2.54
+inch = 24
+cm = inch * per_inch
+
+# 文字列で説明を加える
+desc = f"{inch}インチ={cm:.1f}センチ"
+print(desc)
